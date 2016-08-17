7837412ccdd80296f23600dc6349c95762c1fb19
julianamorota <juliana.morota@mstech.com.br>
2016-08-17 12:17:05 -0300

    alteracoes no editar

---
+++ Projetos/ToDo List/controller.js
@@ -0,0 +1,51 @@
+app.controller("toDoListCtrl", function($scope){
+
+  $scope.app = "To-Do List";
+  $scope.itens = [];
+
+  /**
+   * @description  adiciona tarefas no array
+   * @param {object} item  model de tarefa
+   */
+  $scope.adicionarItem = function(item){
+    item.situacao="pendente";
+    $scope.itens.push(angular.copy(item));
+    delete $scope.item;
+  };
+
+  /**
+   * @description  editar
+   * @param {object} itens  model de tarefas
+   */
+  $scope.editarItem = function(item){
+
+  };
+
+  /**
+   * @description  o que é a fn
+   * @param {object} itens  model de tarefas
+   */
+  $scope.alterarItem = function(item){
+
+  };
+
+/**
+ * @description  o que é a fn
+ * @param {object} itens  model de tarefas
+ */
+  $scope.excluirItem = function(item){
+
+    /*$scope.itens = itens.filter(function(item){
+      if(!item.selecionado)
+        return item;
+    });
+    */
+  };
+
+  $scope.editarItem = function(item){
+    $scope.editar = item;
+    console.log($scope.itens);
+  };
+
+
+});
