dfd96d79f68730661c491493df546f33ddc178c8
julianamorota <juliana.morota@mstech.com.br>
2016-08-19 11:20:31 -0300

    changes

--- Projetos/ToDo List/controller.js
+++ Projetos/ToDo List/controller.js
@@ -1,21 +1,32 @@
 (function () {
+'use strict';
 
 angular
   .module('toDoList')
   .controller("toDoListCtrl", todo);
-
   angular.$inject = ['$scope'];
 
 function todo ($scope) {
-    $scope.app = "To-Do List";
+    $scope.app = "To-Do List"; //nome do app
     $scope.itens = [];
     $scope.situacoes = [
       {descricao: "pendente"},
       {descricao: "finalizada"}
     ];
+    $scope.exibicao = ""; //deixar exibicao "todas" selecionado por default
+
+    if(localStorage["toDo"]){
+      $scope.itens = ('toDo: ', JSON.parse(localStorage.getItem('toDo')));
+    }
+    if(localStorage["toDoSituacao"]){
+      $scope.situacoes = ('toDoSituacao: ', JSON.parse(localStorage.getItem('toDoSituacao')));
+    }
+
+    /*
     $scope.itens = ('toDo: ', JSON.parse(localStorage.getItem('toDo')));
     $scope.situacoes = ('toDoSituacao: ', JSON.parse(localStorage.getItem('toDoSituacao')));
-    $scope.exibicao = ""; //deixar exibicao "todas" selecionado por default
+    */
+
 
     /**
      * @description  adiciona tarefas no array
@@ -33,7 +44,6 @@ function todo ($scope) {
    * @param {object} itens  model de tarefas
    */
     $scope.excluirItem = function(itens){
-
       $scope.itens = itens.filter(function(item){
         if(!item.selecionado)
           return item;
@@ -61,12 +71,21 @@ function todo ($scope) {
   	};
 
     /**
-    * @description  editar e salvar tarefas
-    * @param {object} itens  model de tarefas
+    * @description  cadastro de situação
+    * @param {object} situacao  model de situacao
     */
     $scope.adicionarSituacao = function(situacao) {
       $scope.situacoes.push(angular.copy(situacao));
       $scope.situacao.descricao = "";
+      localStorage.setItem('toDoSituacao', JSON.stringify($scope.situacoes));
+    };
+
+    /**
+    * @description  excluir situação
+    * @param {object} situacao  model de situacao
+    */
+    $scope.excluirSituacao = function(situacao) {
+      $scope.situacoes = splice(situacao, 1);
       localStorage.setItem('toDoSituacao', JSON.stringify($scope.situacoes));
     };
 }
